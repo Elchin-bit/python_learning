bb484fbed072afeddc21e841cdae4fd1cc170389
Elchin Aliev <krogbro@mail.ru>
2025-05-08 22:08:24 +0300

    'practice nested lists'

--- Nested lists.py
+++ Nested lists.py
@@ -1,8 +1,19 @@
-list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
-list1[2][2].append(7000)
 
-print(list1)
+def solve_task_1():
+    print("--- Задача 1 ---")
+    list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+    list1[2][2].append(7000) 
+    print(list1)
+
+def solve_task_2():
+    print("--- Задача 2 ---")
+    list1 = ['a', 'b', ['c', ['d', 'e', ['f', 'g'], 'k'], 'l'], 'm', 'n']
+    sub_list = ['h', 'i', 'j']
+    list1[2][1][2].extend(sub_list)
+    print(list1)
 
 
-
-
+# --- Блок для вызова нужных функций ---
+if __name__ == "__main__":
+    # solve_task_1()
+    solve_task_2()
