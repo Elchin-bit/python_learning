6eb31e8a7e8926d3b7f0945607911747a72d8729
Elchin Aliev <krogbro@mail.ru>
2025-05-08 23:55:52 +0300

    'practice nested lists'

--- Nested lists.py
+++ Nested lists.py
@@ -12,8 +12,17 @@ def solve_task_2():
     list1[2][1][2].extend(sub_list)
     print(list1)
 
+def solve_task_3():
+    print("--- Задача 3 ---")
+    list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+    maximum = []
+    for i in list1:
+        maximum.append(max(i))
+    print(max(maximum))
+
 
 # --- Блок для вызова нужных функций ---
 if __name__ == "__main__":
     # solve_task_1()
-    solve_task_2()
+    # solve_task_2()
+    solve_task_3()
