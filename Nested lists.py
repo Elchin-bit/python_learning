b16851398385a392a0ca92c12fbded2dfb978cf3
Elchin Aliev <krogbro@mail.ru>
2025-05-10 00:55:15 +0300

    'practice tuple'

--- Nested lists.py
+++ Nested lists.py
@@ -33,10 +33,37 @@ def solve_task_4():
     print(total / counter)
 
 
+def solve_task_5():
+    print("--- Задача 5 ---")
+    # numbers = ((10, 10, 10, 12), (30, 45, 56, 45), (81, 80, 39, 32), (1, 2, 3, 4), (90, 10))
+    # averages = []
+    #
+    # for inner_tuple in numbers:
+    #
+    #     current_sum = sum(inner_tuple)
+    #
+    #     current_length = len(inner_tuple)
+    #
+    #     if current_length > 0:
+    #         average = current_sum / current_length
+    #     else:
+    #         average = 0
+    #
+    #     averages.append(average)
+    #
+    # print(averages)
+
+    numbers = ((10, 10, 10, 12), (30, 45, 56, 45), (81, 80, 39, 32), (1, 2, 3, 4), (90, 10))
+
+    averages = [sum(inner_tuple) / len(inner_tuple) if len(inner_tuple) > 0 else 0 for inner_tuple in numbers]
+
+    print(averages)
+
 
 # --- Блок для вызова нужных функций ---
 if __name__ == "__main__":
     # solve_task_1()
     # solve_task_2()
     # solve_task_3()
-    solve_task_4()
+    # solve_task_4()
+    solve_task_5()
