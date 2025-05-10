73bb8bd497383313b1de8051d4a0226694818759
Elchin Aliev <krogbro@mail.ru>
2025-05-10 20:24:12 +0300

    Finalize current practice files

--- sets.py
+++ sets.py
@@ -30,14 +30,26 @@ def solve_task_4():
     set1.difference_update(set2)
     print(*sorted(set1))
 
+
 def solve_task_5():
     print("--- Задача 5 ---")
+    items = [10, '30', 30, 10, '56', 34, '12', 90, 89, 34, 45, '67', 12, 10, 90, 23, '45', 56, '56', 1, 5, '6', 5]
+    items_set = {int(i) for i in items}
+    print(*sorted(items_set))
 
 
+def solve_task_6():
+    print("--- Задача 6 ---")
+    words = ['Plum', 'Grapefruit', 'apple', 'orange', 'pomegranate', 'Cranberry', 'lime', 'Lemon', 'grapes',
+             'persimmon', 'tangerine', 'Watermelon', 'currant', 'Almond']
+    words_set = {i[0].lower() for i in words}
+    print(*sorted(words_set))
 
 
 if __name__ == "__main__":
     # solve_task_1()
     # solve_task_2()
     # solve_task_3()
-    solve_task_4()
+    # solve_task_4()
+    # solve_task_5()
+    solve_task_6()
