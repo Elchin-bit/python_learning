32e35195b8508f5b54c30d41c4de4508ebafb724
Elchin Aliev <krogbro@mail.ru>
2025-05-20 13:17:16 +0300

    "practice func"

--- python_practice/02_functions_practice.py
+++ python_practice/02_functions_practice.py
@@ -11,12 +11,12 @@ def merge_lists2():
         current_line_numbers = [int(c) for c in input().split()]
         all_lines_data.extend(current_line_numbers)
     all_lines_data.sort()
-    print(all_lines_data)
+    return all_lines_data
 
 
 
 if __name__ == "__main__":
     # merge_lists([int(c) for c in input().split()], [int(c) for c in input().split()])
-    merge_lists2()
+    print(merge_lists2())
 
 
