2f55413556432afbc01928959d23a38499004fca
Elchin Aliev <krogbro@mail.ru>
2025-06-01 13:49:27 +0300

    "task to return multiple values"

--- python_practice/02_functions_practice.py
+++ python_practice/02_functions_practice.py
@@ -128,6 +128,7 @@ def is_correct_bracket(text):
 
     return count == 0
 
+
 def convert_to_python_case(text):
     if not text:
         return ""
@@ -142,8 +143,35 @@ def convert_to_python_case(text):
 
     return new_text
 
+
 def get_middle_point(x1, y1, x2, y2):
-    pass
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+    return x, y
+
+
+def get_circle(radius):
+    C = 2 * math.pi * radius
+    S = math.pi * radius ** 2
+
+    return C, S
+
+
+def solve(a, b, c):
+    d = pow(b, 2) - 4 * a * c
+    if d < 0:
+        return False
+    elif d == 0:
+        x = -(b / (2 * a))
+        return x, x
+    else:
+        x1 = (-b - math.sqrt(d)) / (2 * a)
+        x2 = (-b + math.sqrt(d)) / (2 * a)
+        if x1 < x2:
+            return x1, x2
+        else:
+            return x2, x1
+
 
 if __name__ == "__main__":
     # merge_lists([int(c) for c in input().split()], [int(c) for c in input().split()])
@@ -185,5 +213,21 @@ if __name__ == "__main__":
     # print(is_correct_bracket(txt))
 
     # --- Код для задачи: Змеиный регистр ---
-    txt = input()
-    print(convert_to_python_case(txt))
+    # txt = input()
+    # print(convert_to_python_case(txt))
+
+    # --- Код для задачи: Середина отрезка ---
+    # x_1, y_1 = int(input()), int(input())
+    # x_2, y_2 = int(input()), int(input())
+    # x, y = get_middle_point(x_1, y_1, x_2, y_2)
+    # print(x, y)
+
+    # --- Код для задачи: Площадь и длина ---
+    # r = float(input())
+    # length, square = get_circle(r)
+    # print(length, square)
+
+    # --- Код для задачи: Корни управления ---
+    a, b, c = int(input()), int(input()), int(input())
+    x1, x2 = solve(a, b, c)
+    print(x1, x2)
